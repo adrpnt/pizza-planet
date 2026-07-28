21ab8356f5174baf1ceea52c2f1705c57e50c367
Adriano Vasconcelos <pntadr@gmail.com>
2026-07-28 08:39:05 -0300

    Error handling on server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import "dotenv/config";
 
 import cors from "cors";
-import express, { type Express } from "express";
+import express, {
+  type Express,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 
 import { router } from "./routes.ts";
 
@@ -11,5 +16,17 @@ const PORT = process.env.PORT! || 3333;
 app.use(express.json());
 app.use(cors());
 app.use(router);
+
+app.use((error: Error, _: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  res.status(500).json({
+    message: error.message || "Internal Server Error",
+    // Only show stack traces in development mode
+    stack: process.env.NODE_ENV === "development" ? error.stack : undefined,
+  });
+});
 
 app.listen(PORT, () => console.log("SERVER ONLINE ON PORT: " + PORT));
