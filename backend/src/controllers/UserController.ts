5b86d86677482235f68ec75cbd4d35ea4984904c
Adriano Vasconcelos <pntadr@gmail.com>
2026-07-28 08:39:39 -0300

    Create user feature

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -4,11 +4,11 @@ import { User } from "../models/User.ts";
 
 class UserController {
   async store(req: Request, res: Response) {
-    // const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
     const userModel = new User();
-    const user = await userModel.create();
+    const user = await userModel.create({ name, email, password });
 
-    res.json({ message: user });
+    res.json(user);
   }
 }
 
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,7 +1,24 @@
+import { hash } from "bcryptjs";
+
+import { type UserType } from "../types/UserType.ts";
+import prismaClient from "../prisma/index.ts";
+
 class User {
-  async create() {
-    console.log("EXECUTANDO USER SERVICE CREATE.");
-    return "USER CREATED!";
+  async create({ name, email, password }: UserType) {
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: { email: email },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+    const user = await prismaClient.user.create({
+      data: { name, email, password: passwordHash },
+    });
+
+    return { message: "User created successfully", user };
   }
 }
 
