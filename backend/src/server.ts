20bfd94a0dfbba237acf138436c01b852e615abf
Adriano Vasconcelos <pntadr@gmail.com>
2026-07-28 07:17:37 -0300

    Prisma migration and configuration client

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
+import "dotenv/config";
+
 import cors from "cors";
 import express, { type Express } from "express";
 
 import { router } from "./routes.ts";
-
-import "dotenv/config";
 
 const app: Express = express();
 const PORT = process.env.PORT! || 3333;
